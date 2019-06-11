4071bb5e0976b2492ace888eb2541d0555307bb9
Rob <rklaumbach@gmail.com>
2019-06-11 00:13:10 -0400

    added grid view

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -2,16 +2,26 @@ package com.example.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.example.coderswag.Adapters.ProductsAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
